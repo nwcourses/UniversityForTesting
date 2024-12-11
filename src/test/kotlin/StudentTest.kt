45aeb104d3c5576e5825840b5ace4ca3b530f4bf
Nick Whitelegg <nickw4426@gmail.com>
2024-12-11 11:32:03 +0100

    Added template test

---
+++ src/test/kotlin/StudentTest.kt
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class StudentTest {
+    @Test
+    fun testTwoNumbersEqual() {
+        val a = 1
+        val b = 1
+        assertEquals(a, b)
+    }
+}
